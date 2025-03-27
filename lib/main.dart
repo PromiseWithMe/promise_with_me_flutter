90697178075dd45a75a32fe12e76555a7c9215d1
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-27 19:20:20 +0900

    feat(env): .env 파일을 .gitignore에 추가하고 dotenv 패키지 사용 feat(main.dart): 환경 변수를 비동기적으로 로드하도록 main 함수 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:promise_with_me_flutter/presentation/splash/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
@@ -29,7 +33,7 @@ class MyApp extends StatelessWidget {
           ),
           scaffoldBackgroundColor: Colors.white,
         ),
-        home: SplashScreen()
+        home: SplashScreen(),
       ),
     );
   }
