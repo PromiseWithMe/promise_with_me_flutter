e0376644c19d7f5b64e83915fe72d369ea48d9ef
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-27 19:20:54 +0900

    feat(bloc): BlocState 및 ExceptionResponseModel 클래스 추가 블록 상태 관리를 위한 새로운 상태 클래스와 예외 응답 모델을 추가하여 상태 관리 및 오류 처리를 개선한다.

---
+++ lib/core/bloc/bloc_state.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'exception_response_model.dart';
+
+enum BlocStateEnum { empty, loading, error, loaded }
+
+abstract class BlocState<T> {
+  BlocStateEnum blocState;
+  T? valueOfNull;
+  DioException? errorOfNull;
+
+  BlocState({required this.blocState, this.valueOfNull, this.errorOfNull});
+
+  T get value => valueOfNull!;
+
+  ExceptionResponseModel get error =>
+      ExceptionResponseModel.fromJson(errorOfNull!.response!);
+}
+
+class Empty<T> extends BlocState<T> {
+  Empty() : super(blocState: BlocStateEnum.empty);
+}
+
+class Loading<T> extends BlocState<T> {
+  Loading() : super(blocState: BlocStateEnum.loading);
+}
+
+class Error<T> extends BlocState<T> {
+  final DioException exception;
+
+  Error({required this.exception})
+    : super(blocState: BlocStateEnum.error, errorOfNull: exception);
+}
+
+class Loaded<T> extends BlocState<T> {
+  final T? data;
+
+  Loaded({required this.data})
+    : super(blocState: BlocStateEnum.loaded, valueOfNull: data);
+}
---
+++ lib/core/bloc/exception_response_model.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+
+class ExceptionResponseModel {
+  final int statusCode;
+  final String message;
+  final String path;
+
+  const ExceptionResponseModel({
+    required this.statusCode,
+    required this.message,
+    required this.path,
+  });
+
+  factory ExceptionResponseModel.fromJson(Response<dynamic> response) {
+    return ExceptionResponseModel(
+      statusCode: response.data['statusCode'],
+      message: response.data['message'],
+      path: response.data['path'],
+    );
+  }
+}
